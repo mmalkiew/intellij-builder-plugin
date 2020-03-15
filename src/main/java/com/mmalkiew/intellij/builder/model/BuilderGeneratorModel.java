a253fe211b676dc93a84bbd4a7a4ec507bb7acb1
mmalkiew <michal.malkiewicz@uniteam.pl>
2020-03-15 17:11:10 +0100

    show member chooser view

--- src/main/java/com/mmalkiew/intellij/builder/model/BuilderGeneratorModel.java
+++ src/main/java/com/mmalkiew/intellij/builder/model/BuilderGeneratorModel.java
@@ -1,5 +1,36 @@
 package com.mmalkiew.intellij.builder.model;
 
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiFile;
+
 public class BuilderGeneratorModel {
 
+    private Project project;
+    private Editor editor;
+    private PsiFile file;
+
+    public BuilderGeneratorModel() {
+        /**
+         * should be emptty
+         */
+    }
+
+    public void updateModel(Project project, Editor editor, PsiFile file) {
+        this.project = project;
+        this.editor = editor;
+        this.file = file;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public Editor getEditor() {
+        return editor;
+    }
+
+    public PsiFile getFile() {
+        return file;
+    }
 }
--- src/main/java/com/mmalkiew/intellij/builder/view/BuilderGeneratorView.java
+++ src/main/java/com/mmalkiew/intellij/builder/view/BuilderGeneratorView.java
@@ -1,12 +1,46 @@
 package com.mmalkiew.intellij.builder.view;
 
+import com.intellij.codeInsight.generation.PsiFieldMember;
+import com.intellij.ide.util.MemberChooser;
+import com.intellij.openapi.application.ApplicationManager;
 import com.mmalkiew.intellij.builder.model.BuilderGeneratorModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class BuilderGeneratorView {
+import javax.swing.*;
+
+public class BuilderGeneratorView implements Runnable {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BuilderGeneratorView.class);
+
+    private static final String TITLE = "Builder options";
 
     private final BuilderGeneratorModel model;
 
     public BuilderGeneratorView(BuilderGeneratorModel model) {
         this.model = model;
+    }
+
+    public void showView() {
+        ApplicationManager.getApplication()
+                          .runWriteAction(this);
+    }
+
+    @Override
+    public void run() {
+        final JCheckBox[] optionCheckBoxes = new JCheckBox[0];
+
+        final PsiFieldMember[] memberArray = new PsiFieldMember[0];
+
+        final MemberChooser<PsiFieldMember> chooser = new MemberChooser<PsiFieldMember>(memberArray,
+                                                                                        false, // allowEmptySelection
+                                                                                        true,  // allowMultiSelection
+                                                                                        model.getProject(), null, optionCheckBoxes);
+
+        chooser.setTitle(TITLE);
+        chooser.selectElements(memberArray);
+        if (chooser.showAndGet()) {
+            chooser.getSelectedElements();
+        }
     }
 }
